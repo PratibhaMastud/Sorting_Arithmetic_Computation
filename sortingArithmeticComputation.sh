c9be4c244913bc5d492fe16d6bd6c6dcff58209d
PratibhaMastud <karandepratibha999@gmail.com>
2020-10-19 23:39:03 +0530

    Store Every Arithmetic Computation Results in dictionary

--- sortingArithmeticComputation.sh
+++ sortingArithmeticComputation.sh
@@ -1,14 +1,29 @@
 #! /bin/bash
-# Arithmatic Computation Problem
+# Store Every Arithmetic Computation Results in dictionary
 #Author:Pratibha Mastud
 
 
-echo "Enter three different number :"
+echo "Enter three different num"
 read a
 read b
 read c
 
-Result=$(($a*$b+$c))
-echo "Result is :" $Result
+Result1=$(($a+$b*$c))
+Result2=$(($a*$b+$c))
+Result3=$(($a/$b+$c))
+Result4=$(($a%$b+$c))
 
-	
+	#! /bin/bash
+
+declare -A result
+result[a]=0
+result[b]=0
+result[c]=0
+result[d]=0
+
+    result=( [a]=$Result1 [b]=$Result2 [c]=$Result3 [d]=$Result4 )
+    echo "All values in Result Dictionary : "${result[@]}
+
+
+
+
