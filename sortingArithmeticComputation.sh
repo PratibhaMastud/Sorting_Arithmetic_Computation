c7ec07ba7b6bb02886127093e4535bd70efb5894
PratibhaMastud <karandepratibha999@gmail.com>
2020-10-20 16:16:59 +0530

    Added:Sort the Computtion Result Ascending Order.

--- sortingArithmeticComputation.sh
+++ sortingArithmeticComputation.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-# Sort Computation result in descending order.
+# Sort Computation result in ascending order.
 #Author:Pratibha Mastud
 
 
@@ -13,26 +13,24 @@ Result2=$(($a*$b+$c))
 Result3=$(($a/$b+$c))
 Result4=$(($a%$b+$c))
 
-
 arr=($Result1 $Result2 $Result3 $Result4)
 echo "Original results array: " ${arr[*]}
+#Sorting Ascending order.
 temp=0
-#Sorting Descending order.
 for (( i=0; i<4; i++ ))
 do
-	for (( j=$i; j<4; j++ ))
-	do
-		if [ ${arr[$i]} -lt ${arr[$j]} ];
-		then
-			temp=${arr[$i]}
-			${arr[$i]}=${arr[$j]}
-			${arr[$j]}=$temp
-		fi
-	done
-done
+        for (( j=0; j<4-i; j++ ))
+        do
+                if [ ${arr[j]} -gt ${arr[$((j+1))]} ]
+                then
+                        #swap
+                        temp=${arr[j]}
+                        arr[$j]=${arr[$((j+1))]}
+                        arr[$((j+1))]=$temp
 
-echo -e "\nSorted Numbers Descending Order :"
-for (( i=0; i<4; i++ ))
-do
-	echo ${arr[$i]}
+                fi
+        done
 done
+echo "Sorting Ascending order :"
+echo ${arr[*]}
+
